434a47ab05f71767a8f5cd62353e0b608234e8fd
thanhvisuperman <1851050187vi@ou.edu.vn>
2022-03-16 16:38:52 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './Login/Login';
-
+import Home from './Home/Home'
 function App() {
   return (
-   <Login/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />}></Route>
+        <Route path="/product" element={<Home />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
